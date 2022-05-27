3b53a54a2823d958bdf4f9a9f620d70a073e720c
adnankadalayi <adnankadalayi90@gmail.com>
2022-05-27 09:20:48 +0530

    edited 3

--- Ekart/settings.py
+++ Ekart/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,7 +148,7 @@ STATICFILES_FINDERS = [
 
 ]
 STATIC_URL = 'static/'
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 # STATICFILES_DIRS = [
 #     os.path.join('static')
 # ]
